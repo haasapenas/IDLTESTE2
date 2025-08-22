356abf35c45a7125bbf55813c1f6b304dc0f944a
haasapenas <wellingtonhaas@gmail.com>
2025-08-21 22:30:53 -0300

    Update useJogos.js

--- src/hooks/useJogos.js
+++ src/hooks/useJogos.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react'
-import { jogosService } from '../services/jogosService'
+import { jogosService } from '../lib/jogosService'
 import { toast } from 'react-hot-toast'
 import { useAuth } from '../contexts/AuthContext'
 
